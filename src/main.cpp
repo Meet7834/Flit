01f67c51b070540441c78068f92bb3d687f6dea1
Meet7834 <proobmeet7834@gmail.com>
2024-04-02 01:28:43 +0530

    refactored tokenization class

--- src/main.cpp
+++ src/main.cpp
@@ -3,77 +3,7 @@
 #include <sstream>
 #include <optional>
 #include <vector>
-
-// These are all our token types
-enum class TokenType {
-    _return,
-    int_lit,
-    semi
-};
-
-struct Token {
-    TokenType type;
-    std::optional<std::string> value{}; // this is optional
-};
-
-// tokenizer: this function will read the string and make a vector with all the tokens
-std::vector<Token> tokenize(const std::string &str) {
-
-    std::vector<Token> tokens; // this will store all the tokens in order
-    std::string buff; // temporary string buffer to store the current token
-
-    for (int i = 0; i < str.length(); i++) {
-        char c = str.at(i);
-
-        // as the token can't start with numeric we will start with alpha only
-        if (std::isalpha(c)) {
-            buff.push_back(c);
-            i++;
-
-            // push the character to buffer until there is alphanumeric after it.
-            while (std::isalnum(str.at(i))) {
-                buff.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            // if the buffer value is certain token type
-            if (buff == "return") {
-                // push the token to vector and clear the buffer
-                tokens.push_back({.type = TokenType::_return});
-                buff.clear();
-                continue;
-            } else { // there is syntax error in the code
-                std::cerr << "Syntax Error in the Code. Fix it and Try again!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        } else if (std::isdigit(c)) { // if token is starting with a number then it must an integer literal
-            buff.push_back(c);
-            i++;
-
-            // iterate until int_lit isn't finished
-            while (std::isdigit(str.at(i))) {
-                buff.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            // push the int_lit to tokens and give it value of buffer
-            tokens.push_back({.type = TokenType::int_lit, .value = buff});
-            buff.clear();
-        } else if (c == ';') { // push the semicolon to the tokens vector
-            tokens.push_back({.type = TokenType::semi});
-        } else if (isspace(c)) { // ignore the space
-            continue;
-        } else {
-            // some syntax error has happened
-            std::cerr << "Syntax Error in the Code. Fix it and Try again!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return tokens;
-}
+#include "tokenization.h"
 
 // parser: this function will convert the tokens to assembly code which will be inside a string
 std::string tokens_to_asm(const std::vector<Token> &tokens) {
@@ -85,10 +15,10 @@ std::string tokens_to_asm(const std::vector<Token> &tokens) {
 
         const Token &token = tokens.at(i);
 
-        if (token.type == TokenType::_return) {
+        if (token.type == TokenType::exit) {
             if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) {
                 if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) {
-                    // for this type of code: "return 10; it will convert it to assembly"
+                    // for this type of code: "exit 10; it will convert it to assembly"
                     output << "    mov rax, 60\n"; // syscall 60 for sys_exit
                     output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
                     output << "    syscall\n";
@@ -119,7 +49,8 @@ int main(int argc, char *argv[]) {
     } // after this bracket the file will be closed
 
     // now lets get all the tokens
-    std::vector<Token> tokens = tokenize(contents);
+    Tokenizer tokenizer(std::move(contents));
+    std::vector<Token> tokens = tokenizer.tokenize();
     {
         // this will make an output file with assembly code
         std::fstream file("out.asm", std::ios::out);
---
+++ src/tokenization.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// These are all our token types
+enum class TokenType {
+    exit, int_lit, semi
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value{}; // this is optional
+};
+
+class Tokenizer {
+public:
+    // explicit because it shouldn't accidentally convert string into a tokenizer
+    inline explicit Tokenizer(std::string src) : m_src(std::move(src)) {
+
+    }
+
+    // tokenizer: this function will read the string and make a vector with all the tokens
+    inline std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::string buff; // temporary string buffer to store the current token
+
+        while (peek().has_value()) {
+            if (std::isalpha(peek().value())) {
+                // as the token can't start with numeric we will start with alpha only
+                buff.push_back(consume());
+
+                while (peek().has_value() && std::isalnum(peek().value())) {
+                    buff.push_back(consume()); // push the character to buffer until there is alphanumeric after it.
+                }
+
+                // if the buffer value is a keyword
+                if (buff == "exit") {
+                    // push the token to vector and clear the buffer
+                    tokens.push_back({.type = TokenType::exit});
+                    buff.clear();
+                    continue;
+                } else { // there is syntax error in the code
+                    std::cerr << "Syntax Error in the Code. Fix it and Try again!" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                continue;
+            } else if (std::isdigit(peek().value())) {
+                // if token is starting with a number then it must an integer literal
+                buff.push_back(consume());
+
+                while (peek().has_value() && std::isdigit(peek().value())) {
+                    buff.push_back(consume());
+                }
+
+                // push the int_lit to tokens and give it value of buffer
+                tokens.push_back({.type = TokenType::int_lit, .value = buff});
+                buff.clear();
+                continue;
+            } else if (peek().value() == ';') {
+                tokens.push_back({.type = TokenType::semi});
+                consume();
+                continue;
+            } else if (isspace(peek().value())) {
+                consume();
+                continue;
+            } else {
+                // some syntax error has happened
+                std::cerr << "Syntax Error in the Code. Fix it and Try again!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        m_index = 0; // in-case we need to use this again
+        return tokens;
+    }
+
+private:
+    // nodiscard means compiler will give a warning if the return value isn't stored/used as it's a constant method. PS: it was suggested by CLion LOL :)
+    // basically if you are not using the return value then it's not doing anything hence [[nodiscard]]
+    [[nodiscard]] std::optional<char> peek(int ahead = 1) const {
+        // it's a constant method which means it isn't modifying any of its members, so it makes it's only useful for returning value
+
+        if (m_index + ahead > m_src.length()) {
+            return {};
+        } else {
+            return m_src.at(m_index);
+        }
+    }
+
+    char consume() {
+        return m_src.at(m_index++);
+    }
+
+    const std::string m_src;
+    int m_index = 0;
+};
