b3c1fc747550e57fb37e0548e961c10da3afde50
Meet7834 <proobmeet7834@gmail.com>
2024-04-09 20:43:25 +0530

    variables assigning completed

--- src/generator.h
+++ src/generator.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <unordered_map>
 #include "parser.h"
 
 class Generator {
 private:
     const NodeProg m_prog;
     std::stringstream m_output;
+    size_t m_stack_size = 0;
+
+    struct Var {
+        size_t stack_loc;
+    };
+    std::unordered_map<std::string, Var> m_vars{};
+
+    void push(const std::string &reg) {
+        m_output << "    push " << reg << "\n";
+        m_stack_size++;
+    }
+
+    void pop(const std::string &reg) {
+        m_output << "    pop " << reg << "\n";
+        m_stack_size++;
+    }
 
 public:
     // this constructor moves the given argument to private member root
@@ -20,11 +37,24 @@ public:
 
             void operator()(const NodeExprIntLit &expr_int_lit) {
                 gen->m_output << "    mov rax, " << expr_int_lit.int_lit.value.value() << "\n";
-                gen->m_output << "    push rax \n";
+                gen->push("rax");
             }
 
             void operator()(const NodeExprIdent &expr_ident) {
-                // TODO
+
+                // if the given identifier doesn't exist
+                if (!gen->m_vars.contains(expr_ident.ident.value.value())) {
+                    std::cerr << "Undeclared Identifier " << expr_ident.ident.value.value() << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+
+                const auto &var = gen->m_vars.at(expr_ident.ident.value.value());
+                // we will find the offset and then copy the variable to the top of the stack
+                std::stringstream offset;
+                // we are multiplying by 8 to convert the integer 64 bits to binary
+                offset << "QWORD [rsp + " << (gen->m_stack_size - var.stack_loc - 1) * 8 << "]\n";
+                gen->push(offset.str());
+
             }
         };
 
@@ -41,12 +71,18 @@ public:
             void operator()(const NodeStmtExit &stmt_exit) const {
                 gen->gen_expr(stmt_exit.expr);
                 gen->m_output << "    mov rax, 60\n";
-                gen->m_output << "    pop rdi\n"; // pop off the stack into the rdi register
+                gen->pop("rdi");
                 gen->m_output << "    syscall\n";
             }
 
             void operator()(const NodeStmtLet &stmt_let) {
+                if (gen->m_vars.contains(stmt_let.ident.value.value())) {
+                    std::cerr << "Identifier already used: " << stmt_let.ident.value.value() << std::endl;
+                    exit(EXIT_FAILURE);
+                }
 
+                gen->m_vars.insert({stmt_let.ident.value.value(), Var{.stack_loc = gen->m_stack_size}});
+                gen->gen_expr(stmt_let.expr);
             }
         };
 
